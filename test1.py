4c1e8c1fda6d1d50b029f74b94b6b8eea42b8ca3
melat kesete <165406336+developer123k@users.noreply.github.com>
2024-04-05 18:28:39 +0300

    i have uploaded one file

---
+++ test1.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen()
+turtle.begin_fill()
+turtle.fillcolor("red")
+i = 0
+while i < 4:
+    turtle.forward(300)
+    turtle.left(90)
+    i += 1
+turtle.end_fill()
+    
+turtle.mainloop()
+
+
